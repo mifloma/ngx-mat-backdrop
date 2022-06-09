d54bb9e1c1e97422055ba350efe6cc7bf864e895
Michael <michael.flottmann@msn.com>
2022-06-09 08:39:33 +0200

    Clean up files

--- projects/ngx-mat-backdrop/src/lib/backdrop.spec.ts
+++ projects/ngx-mat-backdrop/src/lib/backdrop.spec.ts
@@ -6,8 +6,8 @@ describe('Backdrop', () => {
     // arrange
     await render('<h2 data-testid="test">test</h2>');
     // assert
-    // const heading = await screen.findByTestId('test');
-    const heading = await screen.findByRole('heading');
+    const heading = await screen.findByTestId('test');
+    // const heading = await screen.findByRole('heading');
     expect(heading).toBeInTheDocument();
   });
 
